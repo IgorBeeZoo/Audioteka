9e4e44ebb4c2eb719b716d1db30e6e6bb373143d
IgorBeeZoo <igorbezhuk@mail.ru>
2024-09-29 15:21:01 +0300

    Написана минимальная фунциональность класса Полка #1

--- Domain/Class1.cs
+++ Domain/Class1.cs
@@ -1,7 +1,33 @@
-﻿namespace Domain
-{
-    public class Class1
-    {
+﻿// <copyright file="Class1.cs" company="Васильева М.А.">
+// Copyright (c) Васильева М.А.. All rights reserved.
+// </copyright>
 
+namespace Domain
+{
+    /// <summary>
+    /// Класс полка
+    /// </summary>
+    public sealed class Shelf
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Shelf"/>.
+        /// </summary>
+        /// <param name="name"> Название полки </param>
+        /// <exception cref="ArgumentNullException">Если название <see langword="null"> </exception>
+        public Shelf(string name)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public Guid Id { get; }
+        /// <summary>
+        /// Название полки
+        /// </summary>
+        public string Name { get; }
+
+        public override string ToString() => $"{this.Name}";
     }
 }
