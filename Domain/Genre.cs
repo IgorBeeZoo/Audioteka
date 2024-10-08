01df71a58c4d6feea5c1b412e6700b0e865df697
IgorBeeZoo <igorbezhuk@mail.ru>
2024-10-08 20:20:37 +0300

    Созданы атрибуты для класа Genre

--- Domain/Genre.cs
+++ Domain/Genre.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain
 {
-    public class Genre
+    /// <summary>
+    /// Класс жанр.
+    /// </summary>
+    public sealed class Genre : IEquatable<Genre>
     {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса <see cref="Genre"/>.
+        /// </summary>
+        /// <param name="name"> Название полки. </param>
+        /// <exception cref="ArgumentNullException">Если название <see langword="null"> </exception>.
+        public Genre(string name)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public Guid Id { get; }
+
+        /// <summary>
+        /// Название полки.
+        /// </summary>
+        public string Name { get; }
+
+        /// <inheritdoc/>
+        public bool Equals(Genre? other)
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return this.Name == other.Name;
+        }
+
+        /// <inheritdoc/>
+        public override string ToString() => $"{this.Name}";
+
+        /// <inheritdoc/>
+        public override bool Equals(object? obj)
+        {
+            return this.Equals(obj as Genre);
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode() => this.Name.GetHashCode();
     }
 }
