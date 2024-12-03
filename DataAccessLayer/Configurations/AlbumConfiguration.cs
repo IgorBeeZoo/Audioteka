acc91c2e5910547a34439983f0de221f274a3ff5
IgorBeeZoo <igorbezhuk@mail.ru>
2024-12-03 18:13:48 +0300

    Добавлены конфигурации, миграции, измменен класс Album

---
+++ DataAccessLayer/Configurations/AlbumConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Configurations
+{
+    using Domain;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public sealed class AlbumConfiguration : IEntityTypeConfiguration<Album>
+    {
+        public void Configure(EntityTypeBuilder<Album> builder)
+        {
+            _ = builder.HasKey(album => album.Id);
+            _ = builder.Property(album => album.Name)
+                .IsRequired();
+            _ = builder.Property(album => album.Date_Release)
+                .IsRequired();
+            _ = builder.Property(album => album.Shelf)
+                .IsRequired(false);
+            _ = builder.Property(album => album.Author)
+                .IsRequired();
+            _ = builder.Property(album => album.Genre)
+                .IsRequired();
+            _ = builder.Property(album => album.Songs)
+                .WithMany(album => album.Albums);
+        }
+    }
+}
